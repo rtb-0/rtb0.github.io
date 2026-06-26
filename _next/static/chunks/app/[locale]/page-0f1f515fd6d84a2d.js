e0b72bb33b1840bf4e8d456e4fcda9a6461c1aa4
demdxx <demdxx@gmail.com>
2026-06-26 22:00:10 +0000

    Deploying to gh-pages from @ rtb-0/rtb0.github.io@818d06c7dfab164f667a67d45add5ffc71424110 🚀

---
+++ _next/static/chunks/app/[locale]/page-0f1f515fd6d84a2d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[465],{2346:(e,s,n)=>{Promise.resolve().then(n.bind(n,7088)),Promise.resolve().then(n.bind(n,3809)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[809,707,356,441,255,358],()=>e(e.s=2346)),_N_E=e.O()}]);
